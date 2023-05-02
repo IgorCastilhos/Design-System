ea8cfa312eeddcec882c2962b056859e5d0a3999
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-05-02 15:58:04 -0300

    ...

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Button, ButtonProps } from '@igorc-ui/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 
 export default {
