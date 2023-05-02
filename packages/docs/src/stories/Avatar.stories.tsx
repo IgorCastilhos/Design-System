8bb0b28b7f7a534df943788d2a49fd03c87f9f11
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-05-02 15:14:36 -0300

    Adding changes to all the components

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/IgorCastilhos.png',
     alt: 'Igor Castilhos',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
