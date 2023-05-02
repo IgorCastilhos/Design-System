d6089acddb4dfa5b3b1baf3cfd357300dce9fa8a
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-05-02 17:23:32 -0300

    fix: Downgrading Storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,10 +1,15 @@
 module.exports = {
-  "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm", '@storybook/addon-a11y'],
-  "framework": {
-    name: "@storybook/react-vite",
-    options: {}
-  },
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
+  ],
+  "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-vite"
   },
@@ -13,11 +18,9 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/Design-System'
+      config.base = '/Design-System/'
     }
+
     return config
-  },
-  docs: {
-    autodocs: true
   }
-};
+}
