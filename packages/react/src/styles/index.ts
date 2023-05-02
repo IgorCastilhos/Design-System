5bee0f50ef119309eca67dd5e552c5f442abd357
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-05-02 15:53:18 -0300

    Transformating doc files

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@igorc-ui/tokens'
+} from '@ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
